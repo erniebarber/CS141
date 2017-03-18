38859732ecde819d27937d44a5b9dc7d651576b3
Ernie Barber <erniebarber@me.com>
2017-03-18 14:01:52 -0400

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,18 @@
+/*
+Had to post it!
+First Java script...
+
+Ernie Barber
+CS141
+Fall 2016 
+*/
+
+
+public class HelloWorld 
+{
+    public static void main(String[] args)
+   {
+     System.out.println("Hello World!");
+   }
+}
+
