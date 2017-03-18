e22709a2419019493ea66cb5619e419b60df5b39
Ernie Barber <erniebarber@me.com>
2017-03-18 13:59:30 -0400

    Add files via upload

---
+++ Homework6.java
@@ -0,0 +1,40 @@
+/*
+HW 6 sorts input Strings (names) into
+alphabetical order with a for loop and 
+the Collections class.   
+
+Ernie Barber
+CS141
+Fall 2016 
+*/
+
+import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.Collections;
+
+public class Homework6
+{
+   public static void main(String[] args)
+   {
+   
+   //variables
+   String input;
+   ArrayList<String> names = new ArrayList<>();
+   
+   //input
+   for (int counter=1;counter<=3;counter++)
+      {
+         Scanner keyboard = new Scanner(System.in);
+         System.out.println("Please enter a name");
+         input = keyboard.nextLine();
+         names.add(input);//adds name to ArrayList names
+      }
+   
+   //sort names
+   Collections.sort(names);//sorts 
+   
+   //output
+   for (int i = 0; i < names.size(); i++)//{} not needed
+	    System.out.println(names.get(i));
+   }  
+}
