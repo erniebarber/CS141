6a16e583b80c26f0f050aa0aed54de9440e5057a
Ernie Barber <erniebarber@me.com>
2017-03-18 12:57:33 -0400

    Add files via upload

---
+++ ClockSeconds.java
@@ -0,0 +1,31 @@
+/*
+Printout each second of a 12 hour clock 
+using nested for loops.
+
+Ernie Barber
+CS141
+Fall 2016
+*/
+
+public class ClockSeconds
+{
+   public static void main(String[] args)
+   {
+   //variables
+   int hour;
+   int min;
+   int sec;
+   
+   for(hour = 0; hour <= 12; hour++)
+   {
+      for(min = 0; min <= 59; min++)
+      {
+         for(sec=0; sec<= 59; sec++)
+         {
+            System.out.printf("%02d:%02d:%02d\n", hour,min,sec);
+         }
+      }
+   }
+
+   }   
+}
